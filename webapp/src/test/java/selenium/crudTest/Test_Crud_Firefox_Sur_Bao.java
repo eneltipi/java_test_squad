27cdb619a278edf4379d4bec11dd6c2ab7283f35
eneltipi <nguyenletienbao99@gmail.com>
2021-03-07 14:52:02 +0700

    Thầy Bảo Commit Dằn Mặt Đọt Lê

--- webapp/src/test/java/selenium/crudTest/Test_Crud_Firefox_Sur_Bao.java
+++ webapp/src/test/java/selenium/crudTest/Test_Crud_Firefox_Sur_Bao.java
@@ -24,6 +24,9 @@ public class Test_Crud_Firefox_Sur_Bao {
 	WebElement roleInsert;
 	WebElement insertSubmit;
 
+	String user = "adsaasf@gmail.co";
+	String pass = "fffffff";
+
 	@Before
 	public void starting() {
 		try {
@@ -45,7 +48,6 @@ public class Test_Crud_Firefox_Sur_Bao {
 	@Test
 	public void checkInsert() throws InterruptedException {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-
 		inputNameInsert = driver.findElement(By.name("email"));
 		inputPassInsert = driver.findElement(By.name("password"));
 		inputFnameInsert = driver.findElement(By.name("fullname"));
@@ -53,17 +55,14 @@ public class Test_Crud_Firefox_Sur_Bao {
 		roleInsert = driver.findElement(By.id("roleSelection"));
 		String tableSize = (String) js.executeScript("return gaugau.tableSize()");
 		Select objSelect = new Select(roleInsert);
-		inputNameInsert.sendKeys("dit me no");
-		inputPassInsert.sendKeys("aaaa");
-		inputFnameInsert.sendKeys("aaaa");
-		inputPhoneInsert.sendKeys("aaaa");
+		inputNameInsert.sendKeys(user);
+		inputPassInsert.sendKeys(pass);
+		inputFnameInsert.sendKeys("54545");
+		inputPhoneInsert.sendKeys("5656575");
 		objSelect.selectByVisibleText("Admin");
-
 		insertSubmit = driver.findElement(By.name("insertSubmit"));
-
 		insertSubmit.click();
 		Thread.sleep(2000);
-
 		String newTableSize = (String) js.executeScript("return gaugau.tableSize()");
 
 		if (Integer.valueOf(newTableSize) == (Integer.valueOf(tableSize) + 1)) {
@@ -71,6 +70,23 @@ public class Test_Crud_Firefox_Sur_Bao {
 		} else {
 			assertFalse(false);
 		}
+	}
+
+	@Test
+	public synchronized void checkDeleteRowJunit() throws InterruptedException {
+		user = "anh@gmail.com";
+		pass = "123";
+		checkInsert();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("return gaugau.deleteRow()");
+		Thread.sleep(500);
+		String result = driver.switchTo().alert().getText();
+		if (result.equalsIgnoreCase("this is default account"))
+			System.out.println("READ COMMENT ABOVE THIS IF");
+		if (result.equalsIgnoreCase("Account successfully deleted"))
+			assertTrue(true);
+		else if (result.equalsIgnoreCase("Failed to delete account"))
+			assertFalse(false);
 	}
 
 	@Test
@@ -83,19 +99,20 @@ public class Test_Crud_Firefox_Sur_Bao {
 		}
 		int ran = (int) (Math.random() * (Integer.parseInt(tableSize) + 1 - 2) + 2);
 		System.out.println(tableSize + ": tableSize: " + ran + ": ran");
-		String xpath = String.format("/html/body/div[2]/div[1]/table/tbody/tr[%s]", ran);
+		String xpath = String.format("/html/body/div[2]/div[1]/table/tbody/tr[%s]/td[2]", ran);
 		Actions actions = new Actions(driver);
 		WebElement randomRow = driver.findElement(By.xpath(xpath));
 		actions.contextClick(randomRow).perform();
+		System.out.println(randomRow + ": randomRow");
 		Thread.sleep(1000);
 		WebElement delbtn = driver.findElement(By.xpath("//*[@id=\"customMenu\"]/a[1]"));
 		delbtn.click();
 		String result = driver.switchTo().alert().getText();
 		System.out.println(result + ": result: ");
-		if (result.equalsIgnoreCase("Account successfully deleted")){
+		if (result.equalsIgnoreCase("Account successfully deleted")) {
 			assertTrue(true);
 		} else {
-			assertFalse(false);
+			assertFalse(true);
 		}
 	}
 
